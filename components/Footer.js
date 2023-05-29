0173f0ac7250e87d79837d9b50eeb093ad952406
Maran.I.Msabeh <maramismail2015@gmail.com>
2023-05-29 15:44:03 +0300

    about page mobile

--- components/Footer.js
+++ components/Footer.js
@@ -4,73 +4,80 @@ import {BsTelephoneFill} from 'react-icons/bs'
 import {MdAccessTimeFilled} from 'react-icons/md'
 import {HiLocationMarker} from 'react-icons/hi'
 import {IoMdMail} from 'react-icons/io'
+import { useTranslation } from 'react-i18next'
 
 const Footer = () => {
+
+   const {t} = useTranslation()
   return (
-  <footer className='bg-[#1768ac] md:pt-[109px]  md:pb-[77px] flex justify-center items-center'>
- <div className='container px-[128px]'>
- <Row className='md:mb-[12px]'>
-  <Col md={{span:8}}>
-     <span className='text-[#fff] font-medium text-[16px]'>@8plus Co</span>
-   
+  <footer className='bg-[#1768ac] md:pt-[109px]  md:pb-[77px] flex justify-center items-center py-[23px]'>
+ <div className='container md:px-[128px] px-[22px]'>
+
+
+
+<Row className="md:mb-[12px]">
+  <Col span={18} md={{span:12}}>
+    <img src="/images/logo.png" />
+    <p className='text-[8px] font-light md:text-[10px] md:font-medium text-[#fff] md:w-[277px] md:mt-[9px]'>{t('home-desc')}</p>
   </Col>
 
-  <Col md={{span:8}}>
-  <div className='flex items-center gap-[13px] text-[#fff] text-[19px]'>
+  <Col span={4} md={{span:12}}>
+    <div className="flex flex-wrap justify-between md:space-y-[16px] space-y-[14px]">
+    <div className='flex items-center gap-[4px] md:gap-[13px] text-[#fff] text-[8px] md:text-[19px] md:w-1/2'>
     
-     <span className='text-[#fff] font-medium text-[16px]'>
-      8pluscompany@info
-      
+    <span className='text-[#fff] font-medium text-[8px] md:text-[19px]'>
+     8pluscompany@info
+     
+     </span>
+     <IoMdMail />
+
+     </div>
+     <div className='flex items-center gap-[4px] md:gap-[13px] text-[#fff] text-[8px] md:text-[19px] md:w-1/2'>
+    
+     <span className='text-[#fff] font-medium text-[8px] md:text-[19px]'>
+      Palestine-Gaza
       </span>
-      <IoMdMail />
+      <HiLocationMarker />
 
-      </div>
- 
-  </Col>
+     </div>
+     <div className='flex items-center gap-[4px]md:gap-[13px] text-[#fff] text-[8px] md:text-[19px] md:w-1/2'>
+    
+     <span className='text-[#fff] font-medium text-[8px] md:text-[19px]'>
+      Palestine - Gaza</span>
+      <HiLocationMarker />
 
+     </div>
+     <div className='flex items-center gap-[4px]md:gap-[13px] text-[#fff] text-[8px] md:text-[19px] md:w-1/2'>
+    
+    <span className='text-[#fff] font-medium text-[8px] md:text-[19px]'>
+     8pluscompany@info
+     
+     </span>
+     <IoMdMail />
 
-  <Col md={{span:8}}>
-  <div className='flex items-center gap-[13px] text-[#fff] text-[19px]'>
-     <span className='text-[#fff] font-medium text-[16px]'>
+     </div>
+     {/* <div className='flex items-center gap-[4px]md:gap-[13px] text-[#fff] text-[8px] md:text-[19px] md:w-1/2'>
+     <span className='text-[#fff] font-medium text-[8px] md:text-[19px]'>
       Palestine - Gaza</span>
       <HiLocationMarker />
       </div>
-  
-  </Col>
+
+      <div className="flex items-center gap-[4px]md:gap-[13px] text-[#fff] text-[8px] md:text-[19px] md:w-1/2">
+        <span className="text-[#fff] font-medium text-[8px] md:text-[19px]">+972595365842</span>
+        <BsTelephoneFill />
+      </div>
 
 
-</Row>
-
-<Row className='md:mb-[12px]'>
-  <Col md={{span:8}}>
-
-     <span className='text-[#fff] font-medium text-[16px]'>all resver saved</span>
-   
-  </Col>
-
-  <Col md={{span:8}}>
-   
-   <div className='flex items-center gap-[35px] text-[#fff] text-[19px]'>
-     <span className='text-[#fff] font-medium text-[16px]'>
-      +972595365842</span>
-      <BsTelephoneFill />
-   </div>
- 
-  </Col>
-
-
-  <Col md={{span:8}}>
-   <div className='flex items-center gap-[33px] text-[#fff] text-[19px]'>
-     <span className='text-[#fff] font-medium text-[16px]'>
+      <div className='flex items-center gap-[4px]md:gap-[13px] text-[#fff] text-[8px] md:text-[19px]md:w-1/2'>
+     <span className='text-[#fff] font-medium text-[8px] md:text-[19px]'>
       24 h At Work</span>
 
       <MdAccessTimeFilled />
-   </div>
-  
+   </div> */}
+    </div>
   </Col>
-
-
 </Row>
+  
  </div>
   </footer>
   )
