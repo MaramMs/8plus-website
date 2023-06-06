2e464f22e8312a916954d42827d47323178131bc
Maran.I.Msabeh <maramismail2015@gmail.com>
2023-06-06 11:01:24 +0300

    edit style in contact  page

--- pages/contact.js
+++ pages/contact.js
@@ -14,7 +14,7 @@ const contact = () => {
     <div className="pt-[162px] overflow-x-hidden">
 
 
-     <Row className="md:mb-[230px] mb-[108px]">
+     <Row className="md:mb-[230px] mb-[108px] flex-col-reverse 	md:flex-row">
         <Col span={24} md={{ span: 12 }} className="md:px-[128px] px-[19px]">
           <h1 className="text-[#1768ac] text-[16px] md:text-[32px] font-medium">contact us</h1>
 
@@ -141,21 +141,22 @@ const contact = () => {
           </Form>
         </Col>
 
-        <Col span={24} md={{ span: 12 }} className="flex justify-end px-[19px] md:px-0 ">
-          <ul className="bg-[#1768AC] md:h-[477px] h-[284px] px-[37px] py-[39px] rounded-[8px] w-[350px] md:rounded-r-[50px] flex md:py-[127px] md:px-[101px] flex-col md:gap-[71px] gap-[31px] md:w-[507px]">
-            <li className="flex items-center gap-[13px]">
+        <Col span={24} md={{ span: 12 }} className="flex justify-end px-[19px] md:px-0 mb-[122px]">
+          <ul className="bg-[#1768AC] md:h-[477px] h-[284px] px-[37px] py-[39px] rounded-[8px] w-[350px] md:rounded-r-[50px] flex md:py-[61px] md:px-[101px] flex-col  md:w-[507px]">
+          <li className="list-disc text-white text-[14px] font-medium mb-[21px] md:mb-[51px]">{t('form-contact')}</li>
+            <li className="flex items-center gap-[13px] md:mb-[71px] mb-[31px] flex-row-reverse	md:flex-row">
               <HiLocationMarker className="text-[#fff] text-[19px]" />
               <span className="text-[#fff] font-medium text-[16px]">
                 Palestine - Gaza
               </span>
             </li>
-            <li className="flex items-center gap-[13px]">
+            <li className="flex items-center gap-[13px] md:mb-[71px] mb-[31px] flex-row-reverse	md:flex-row">
               <BsTelephoneFill className="text-[#fff] text-[19px]" />
               <span className="text-[#fff] font-medium text-[16px]">
                 +972595365842
               </span>
             </li>
-            <li className="flex items-center gap-[13px]">
+            <li className="flex items-center gap-[13px] md:mb-[71px] mb-[31px] flex-row-reverse	md:flex-row">
               <IoMdMail className="text-[#fff] text-[19px]" />
               <span className="text-[#fff] font-medium text-[16px]">
                 8pluscompany@info
