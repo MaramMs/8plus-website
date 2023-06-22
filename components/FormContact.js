a5fa0eef7fbcc3220f7079db8a9c720b6e075a77
Maran.I.Msabeh <maramismail2015@gmail.com>
2023-06-22 11:01:51 +0300

    post data from contact form

--- components/FormContact.js
+++ components/FormContact.js
@@ -1,13 +1,12 @@
 import { Button, Col, Form, Input, Row, Select, notification } from "antd";
 import React, { useState } from "react";
 
-const FormContact = ({data}) => {
+const FormContact = ({ data }) => {
   const [api, contextHolder] = notification.useNotification();
- 
-  const openNotificationWithIcon = (type,message) => {
-    api[type]({
-      message
 
+  const openNotificationWithIcon = (type, message) => {
+    api[type]({
+      message,
     });
   };
   const handleKeyPress = (e) => {
@@ -23,181 +22,165 @@ const FormContact = ({data}) => {
     }
   };
   const [form] = Form.useForm();
-  const onFinish = async(values) => {
-    const res = await fetch('https://new.8plusit.com/api/post-services' , {
-    method:'POST',  
-    body:JSON.stringify(values),
-      headers:{
-        'content-Type':'application/json'
-      }
+  const onFinish = async (values) => {
+    const res = await fetch("https://new.8plusit.com/api/post-services", {
+      method: "POST",
+      body: JSON.stringify(values),
+      headers: {
+        "content-Type": "application/json",
+      },
     });
     const data = await res.json();
-if(data.status === 'success'){
-  console.log('maram');
-  openNotificationWithIcon('success',data.message)
-}else{
-  openNotificationWithIcon('error',data.message);
-}
-    
+    if (data.status === "success") {
+      console.log("maram");
+      openNotificationWithIcon("success", data.message);
+    } else {
+      openNotificationWithIcon("error", data.message);
+    }
   };
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
 
   const handleSelected = (e) => {
-    form.setFieldsValue({ service_id: e })
-  }
+    form.setFieldsValue({ service_id: e });
+  };
   return (
     <>
-        {contextHolder}
-        <Form
-      form={form}
-      className="md:mt-[39px]"
-      name="basic"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Row span="24" gutter={[36, 10]}>
-        <Col span={24} md={{ span: 12 }}>
-
-<Form.Item
-      name="name"
-      rules={[
-        {
-          required: true,
-          message: "The filed is require",        },
-      ]}
-    >
-      <div className="flex flex-col">
-
-          <label className="text-[#949494] text-[16px]">Name</label>
-      <Input className="border-b-[#D1D1D1] border-t-0 border-r-0 border-l-0 rounded-none"/>
-      </div>
-
-    </Form.Item>
-        </Col>
-
-        <Col span={24} md={{ span: 12 }}>
-          <Form.Item
-            name="email"
-            rules={[
-              {
-                type: "email",
-                message: "The input is not valid E-mail!",
-              },
-              {
-                required: true,
-                message: "Please input your E-mail!",
-              },
-            ]}
-          >
-            <div className="flex flex-col">
-              <label className="text-[#949494] text-[16px]">E-mail</label>
-
-              <Input className="border-b-[#D1D1D1] border-t-0 border-r-0 border-l-0 rounded-none outline-0" />
-            </div>
-          </Form.Item>
-        </Col>
-
-        <Col span={24} md={{ span: 12 }}>
-          <Form.Item
-            name="phone"
-            rules={[
-              {
-                required: true,
-                message: "The filed is require",
-              },
-              {
-                pattern: /^\d+$/,
-                message: "The phone number must only contain digits",
-              },
-            ]}
-          >
-            <div className="flex flex-col">
-              <label className="text-[#949494] text-[16px] ">
-                Phone Number
-              </label>
-              <Input
-                className="border-b-[#D1D1D1] border-t-0 border-r-0 border-l-0 rounded-none outline-0"
-                onKeyPress={handleKeyPress}
-              />
-            </div>
-          </Form.Item>
-        </Col>
-
-        <Col span={24} md={{ span: 12 }}>
-          <Form.Item
-            name="service_id"
-            rules={[
-              {
-                required: true,
-                message: "The filed is require",
-              },
-            ]}
-          >
-         <div className="flex flex-col">
-
-         <label className="text-[#949494] text-[16px]">
-              Type of Services
-            </label>
-            <Select
-          
-            onChange={(e)=> handleSelected(e)}
-              defaultValue="select your service"
-              className="border-b-[#D1D1D1] border-t-0 border-r-0 border-l-0 rounded-none"
-
+      {contextHolder}
+      <Form
+        form={form}
+        className="md:mt-[39px]"
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Row span="24" gutter={[36, 10]}>
+          <Col span={24} md={{ span: 12 }}>
+            <Form.Item
+              name="name"
+              rules={[
+                {
+                  required: true,
+                  message: "The filed is require",
+                },
+              ]}
             >
-              {
-                data.map((item) =>{
-                  return (
-<Option value={item.id}>{item.name}</Option>
-                  )
-                })
-              }
-              
-          
-            </Select>
-         </div>
+              <div className="flex flex-col">
+                <label className="text-[#949494] text-[16px]">Name</label>
+                <Input className="border-b-[#D1D1D1] border-t-0 border-r-0 border-l-0 rounded-none" />
+              </div>
+            </Form.Item>
+          </Col>
 
-          </Form.Item>
-        </Col>
+          <Col span={24} md={{ span: 12 }}>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  type: "email",
+                  message: "The input is not valid E-mail!",
+                },
+                {
+                  required: true,
+                  message: "Please input your E-mail!",
+                },
+              ]}
+            >
+              <div className="flex flex-col">
+                <label className="text-[#949494] text-[16px]">E-mail</label>
 
-        <Col span={24}>
-          <Form.Item
-            name="note"
-            rules={[
-              {
-                required: true,
-                message:"The filed is require",
-              },
-            ]}
+                <Input className="border-b-[#D1D1D1] border-t-0 border-r-0 border-l-0 rounded-none outline-0" />
+              </div>
+            </Form.Item>
+          </Col>
+
+          <Col span={24} md={{ span: 12 }}>
+            <Form.Item
+              name="phone"
+              rules={[
+                {
+                  required: true,
+                  message: "The filed is require",
+                },
+                {
+                  pattern: /^\d+$/,
+                  message: "The phone number must only contain digits",
+                },
+              ]}
+            >
+              <div className="flex flex-col">
+                <label className="text-[#949494] text-[16px] ">
+                  Phone Number
+                </label>
+                <Input
+                  className="border-b-[#D1D1D1] border-t-0 border-r-0 border-l-0 rounded-none outline-0"
+                  onKeyPress={handleKeyPress}
+                />
+              </div>
+            </Form.Item>
+          </Col>
+
+          <Col span={24} md={{ span: 12 }}>
+            <Form.Item
+              name="service_id"
+              rules={[
+                {
+                  required: true,
+                  message: "The filed is require",
+                },
+              ]}
+            >
+              <div className="flex flex-col">
+                <label className="text-[#949494] text-[16px]">
+                  Type of Services
+                </label>
+                <Select
+                  onChange={(e) => handleSelected(e)}
+                  defaultValue="select your service"
+                  className="border-b-[#D1D1D1] border-t-0 border-r-0 border-l-0 rounded-none"
+                >
+                  {data.map((item) => {
+                    return <Option value={item.id}>{item.name}</Option>;
+                  })}
+                </Select>
+              </div>
+            </Form.Item>
+          </Col>
+
+          <Col span={24}>
+            <Form.Item
+              name="note"
+              rules={[
+                {
+                  required: true,
+                  message: "The filed is require",
+                },
+              ]}
+            >
+              <div className="flex flex-col">
+                <label className="text-[#949494] text-[16px]">Note</label>
+
+                <Input className="border-b-[#D1D1D1] border-t-0 border-r-0 border-l-0 rounded-none" />
+              </div>
+            </Form.Item>
+          </Col>
+        </Row>
+        <Form.Item className="flex justify-center md:mt-[95.51px]">
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="md:w-[272px] w-[126px]  h-[48px] bg-[#1768ac] rounded-[24px] text-[#fff] font-semibold text-[14px] p-[12px] flex justify-center items-center"
           >
-          <div className="flex flex-col">
-
-          <label className="text-[#949494] text-[16px]">Note</label>
-
-<Input className="border-b-[#D1D1D1] border-t-0 border-r-0 border-l-0 rounded-none" />
-          </div>
-          </Form.Item>
-        </Col>
-      </Row>
-      <Form.Item className="flex justify-center md:mt-[95.51px]">
-        <Button
-          type="primary"
-          htmlType="submit"
-          className="md:w-[272px] w-[126px]  h-[48px] bg-[#1768ac] rounded-[24px] text-[#fff] font-semibold text-[14px] p-[12px] flex justify-center items-center"
-        >
-          Send
-        </Button>
-      </Form.Item>
-    </Form>
-
+            Send
+          </Button>
+        </Form.Item>
+      </Form>
     </>
-    
   );
 };
 
--- pages/contact.js
+++ pages/contact.js
@@ -8,52 +8,73 @@ import { BsTelephoneFill } from "react-icons/bs";
 import { HiLocationMarker } from "react-icons/hi";
 import { IoMdMail } from "react-icons/io";
 
-const contact = () => {
+const contact = ({
+  resDataServices: { data },
+  resDataHome: { data: homeData },
+}) => {
+  const {
+    site: { site_email, site_phone, site_address },
+  } = homeData;
+  console.log(homeData, "site_email");
   const { t } = useTranslation();
   return (
-<>
-    <div className="pt-[162px] overflow-x-hidden">
+    <>
+      <div className="pt-[162px] overflow-x-hidden">
+        <Row className="md:mb-[230px] mb-[108px] flex-col-reverse 	md:flex-row">
+          <Col span={24} md={{ span: 12 }} className="md:px-[128px] px-[19px]">
+            <h1 className="text-[#1768ac] text-[16px] md:text-[32px] font-medium">
+              contact us
+            </h1>
 
+            <p className="break-words text-[14px] md:text-[16px] text-[#949494] font-[450] mt-[9px] leading-[23px] mb-[36px]">
+              {t("home-desc")}
+            </p>
+            <FormContact data={data} />
+          </Col>
 
-     <Row className="md:mb-[230px] mb-[108px] flex-col-reverse 	md:flex-row">
-        <Col span={24} md={{ span: 12 }} className="md:px-[128px] px-[19px]">
-          <h1 className="text-[#1768ac] text-[16px] md:text-[32px] font-medium">contact us</h1>
-
-          <p className="break-words text-[14px] md:text-[16px] text-[#949494] font-[450] mt-[9px] leading-[23px] mb-[36px]">
-            {t("home-desc")}
-          </p>
-     <FormContact />
-        </Col>
-
-        <Col span={24} md={{ span: 12 }} className="flex justify-end px-[19px] md:px-0 mb-[122px]">
-          <ul className="bg-[#1768AC] md:h-[477px] h-[284px] px-[37px] py-[39px] rounded-[8px] w-[350px] md:rounded-r-[50px] flex md:py-[61px] md:px-[101px] flex-col  md:w-[507px]">
-          <li className="list-disc text-white text-[14px] font-medium mb-[21px] md:mb-[51px]">{t('form-contact')}</li>
-            <li className="flex items-center gap-[13px] md:mb-[71px] mb-[31px] flex-row-reverse	md:flex-row">
-              <HiLocationMarker className="text-[#fff] text-[19px]" />
-              <span className="text-[#fff] font-medium text-[16px]">
-                Palestine - Gaza
-              </span>
-            </li>
-            <li className="flex items-center gap-[13px] md:mb-[71px] mb-[31px] flex-row-reverse	md:flex-row">
-              <BsTelephoneFill className="text-[#fff] text-[19px]" />
-              <span className="text-[#fff] font-medium text-[16px]">
-                +972595365842
-              </span>
-            </li>
-            <li className="flex items-center gap-[13px] md:mb-[71px] mb-[31px] flex-row-reverse	md:flex-row">
-              <IoMdMail className="text-[#fff] text-[19px]" />
-              <span className="text-[#fff] font-medium text-[16px]">
-                8pluscompany@info
-              </span>
-            </li>
-          </ul>
-        </Col>
-      </Row>
-
-    
-    </div>
-</>
+          <Col
+            span={24}
+            md={{ span: 12 }}
+            className="flex justify-end px-[19px] md:px-0 mb-[122px]"
+          >
+            <ul className="bg-[#1768AC] md:h-[477px] h-[284px] px-[37px] py-[39px] rounded-[8px] w-[350px] md:rounded-r-[50px] flex md:py-[61px] md:px-[101px] flex-col  md:w-[507px]">
+              <li className="list-disc text-white text-[14px] font-medium mb-[21px] md:mb-[51px]">
+                {t("form-contact")}
+              </li>
+              <li className="flex items-center gap-[13px] md:mb-[71px] mb-[31px] flex-row-reverse	md:flex-row">
+                <HiLocationMarker className="text-[#fff] text-[19px]" />
+                <span className="text-[#fff] font-medium text-[16px]">
+                  {site_address}{" "}
+                </span>
+              </li>
+              <li className="flex items-center gap-[13px] md:mb-[71px] mb-[31px] flex-row-reverse	md:flex-row">
+                <BsTelephoneFill className="text-[#fff] text-[19px]" />
+                <span className="text-[#fff] font-medium text-[16px]">
+                  {site_phone}
+                </span>
+              </li>
+              <li className="flex items-center gap-[13px] md:mb-[71px] mb-[31px] flex-row-reverse	md:flex-row">
+                <IoMdMail className="text-[#fff] text-[19px]" />
+                <span className="text-[#fff] font-medium text-[16px]">
+                  {site_email}
+                </span>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </div>
+    </>
   );
 };
 
 export default contact;
+
+export async function getServerSideProps() {
+  const resServices = await fetch("https://new.8plusit.com/api/services");
+  const resDataServices = await resServices.json();
+
+  const resHome = await fetch("https://new.8plusit.com/api/home");
+  const resDataHome = await resHome.json();
+
+  return { props: { resDataServices, resDataHome } };
+}
