575c4fecc90001041a0fe7fab8053d12015f8e80
Maran.I.Msabeh <maramismail2015@gmail.com>
2023-06-25 09:13:09 +0300

    fix style

--- components/CardProject.js
+++ components/CardProject.js
@@ -5,7 +5,7 @@ import React from 'react'
 const CardProject = ({filterProject}) => {
   return (
     <>
-          <div className='w-[378px] h-[632px] rounded-[110px] md:rounded-[189px]  overflow-hidden pl-[20px] md:pl-0'>
+          <div className='md:w-[378px] md:h-[632px] rounded-[110px] md:rounded-[189px]  overflow-hidden pl-[20px] md:pl-0'>
           <Link  href={ `/ourworks/${filterProject.id}`} className='rounded-[189px]'>
               <Image src={filterProject.image} alt="slide_image" width={378} height={632} className='object-cover rounded-[189px] w-[378px] h-[632px]'/>
             </Link>
