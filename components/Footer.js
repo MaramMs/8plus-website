3eb5b63783894349a5ad2955cea4d865415e70b0
Maran.I.Msabeh <maramismail2015@gmail.com>
2023-07-03 12:30:48 +0300

    edit style footer

--- components/Footer.js
+++ components/Footer.js
@@ -35,18 +35,18 @@ const Footer = () => {
         <footer className='bg-[#1768ac] bg-[url("/images/footer-img.png")] md:py-[70px] '>
           <Wrapper>
             <Row className="md:mb-[12px]">
-              <Col span={18} md={{ span: 12 }}>
+              <Col span={16} md={{ span: 12 }}>
                 <img
                   src={siteData.site_logo}
                   className="md:w-[140px] md:h-[43px] w-[80px] h-[24.83px] object-cover"
                 />
-                <p className="text-[8px] font-light md:text-[12px] md:font-medium text-[#fff] md:w-[300px] md:mt-[9px] w-[186px] h-[55px]">
+                <p className="text-[8px] font-light md:text-[12px] md:font-medium text-[#fff] md:w-[300px] md:mt-[9px] w-[186px] h-[55px] break-words">
                   {siteData.site_description}
                 </p>
               </Col>
 
-              <Col span={6} md={{ span: 12 }}>
-                <div className="flex flex-wrap justify-between  md:space-y-[16px] space-y-[14px] md:m-0">
+              <Col span={8} md={{ span: 12 }}>
+                <div className="flex flex-wrap md:justify-between  justify-end md:space-y-[16px] space-y-[14px] md:m-0">
                   <div className="flex items-center gap-[4px] md:gap-[13px] text-[#fff] text-[8px] md:text-[19px] md:w-1/2 md:mt-[12px]">
                     <span className="text-[#fff] font-medium text-[8px] md:text-[19px]">
                       {" "}
@@ -62,15 +62,18 @@ const Footer = () => {
                     <HiLocationMarker className="text-[12px] md:text-[14px]" />
                   </div>
 
-                  <div className="flex items-center gap-[4px] md:gap-[15px] text-[#fff] text-[8px] md:text-[19px] md:w-1/2">
-                    <span className="text-[#fff] font-medium text-[8px] md:text-[19px] md:mr-[34px] mr-[13px]">
+              
+
+<div className="flex items-center gap-[4px] md:gap-[13px] text-[#fff] text-[8px] md:text-[19px] md:w-1/2 md:mt-[12px]">
+                    <span className="text-[#fff] font-medium text-[8px] md:text-[19px] mr-[18px]">
+                      {" "}
                       {siteData.site_phone}
                     </span>
                     <BsTelephoneFill className="text-[12px] md:text-[14px]" />
                   </div>
 
                   <div className="flex items-center gap-[5px] md:gap-[10px] text-[#fff] text-[8px] md:text-[19px] md:w-1/2 justify-end">
-                    <span className="text-[#fff] font-medium text-[8px] md:text-[19px] md:mr-[25px] mr-[28px]">
+                    <span className="text-[#fff] font-medium text-[8px] md:text-[19px] md:mr-[25px] ">
                       {siteData.work_hours}
                     </span>
 
