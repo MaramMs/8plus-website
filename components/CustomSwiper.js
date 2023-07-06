7add7c88d120de4b938948d8f3f10127d7c5a398
Maran.I.Msabeh <maramismail2015@gmail.com>
2023-07-06 15:49:00 +0300

    edit slider

--- components/CustomSwiper.js
+++ components/CustomSwiper.js
@@ -1,54 +1,73 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCoverflow, Pagination, Navigation } from "swiper";
+import { EffectCoverflow, Pagination, Navigation,Autoplay } from "swiper";
 import Link from "next/link";
 import CardProject from "./CardProject";
 import { Empty } from "antd";
+import Wrapper from "./Wrapper";
+import { EffectFade } from 'swiper';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const CustomSwiper = ({ portfolios, filterProjects }) => {
   console.log(filterProjects, "filter");
 
   return (
     <Swiper
-      spaceBetween={24}
-      grabCursor={true}
-      centeredSlides={true}
-      loop={true}
-      slidesPerView={3}
-      navigation={{
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-        clickable: true,
+    pagination={{
+      clickable: true,
+    }}
+    spaceBetween={50}
+    // grabCursor={true}
+    // centeredSlides={true}
+    // loop={true}
+    slidesPerView={3}
+    // navigation={{
+    //   nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    //     clickable: true,
+    //   }}
+    //   pagination={{
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    //     dynamicBullets: true,
+    //   }}
+      // breakpoints={{
+      //   640: {
+      //     slidesPerView: 1,
+      //   },
+      //   768: {
+      //     slidesPerView: 2,
+      //   },
+      //   1024: {
+      //     slidesPerView: 3,
+      //   },
+      // }}
+    
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
       }}
-      pagination={{
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
-      }}
-      breakpoints={{
-        640: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        },
-      }}
-      modules={[EffectCoverflow, Pagination, Navigation]}
-      className="swiper_container"
+      modules={[Pagination,Autoplay]}
+      // className="swiper_container"
     >
       {filterProjects.map((filterProject, index) => {
         return (
-          <SwiperSlide key={index}>
+           
+          
+          <SwiperSlide key={index}     className="swiper_container">
             <CardProject filterProject={filterProject} />
           </SwiperSlide>
-        );
+ 
+        )
       })}
 
-      <div className="swiper-pagination"></div>
+      {/* <div className="swiper-pagination"></div> */}
     </Swiper>
+
+  
   );
 };
 
